7c8b86701f48c4c0f5064d6285089a92341f078a
Viraat Aryabumi Reddy <viraat94@gmail.com>
2015-02-20 10:44:02 +0530

    write comments

--- scanner-program.c
+++ scanner-program.c
@@ -2,37 +2,37 @@
 
 int main()
 {
-        FILE *fp1, *fp2;
-        char buff[255], ch;
-        int lineno = 1;
-        int tokenno = 0;
-        char arithop[] = "Arithmetic Operator";
-        char assignop[] = "Assignment Operator";
-        char delim[] = "Delimiter";
-        char compare[] = "Comparision Operator";
+        FILE *fp1, *fp2; //file pointers for input and output file
+        char buff[255], ch; 
+        int lineno = 1; //variable for line number
+        int tokenno = 0; //variable to maintain token number
+        char arithop[] = "Arithmetic Operator"; //string to display arithmetic operator
+        char assignop[] = "Assignment Operator"; //string to display assignment operator
+        char delim[] = "Delimiter"; //string to display delimiter
+        char compare[] = "Comparision Operator"; //string to display comparision operator
 
-        fp1 = fopen("test.txt","r");
-        fp2 = fopen("result.txt","w");
+        fp1 = fopen("test.txt","r"); //open input file in read mode
+        fp2 = fopen("result.txt","w"); //open output file in write mode
 
-        fprintf(fp2, "Line no\t\t Token no\t\t Token\t\t\t Lexeme\n");
+        fprintf(fp2, "Line no\t\t Token no\t\t Token\t\t\t Lexeme\n"); //print the top line in the output file
 
-        while((ch=fgetc(fp1))!=EOF)
+        while((ch=fgetc(fp1))!=EOF) //loop till all characters in input file are exhausted
         {
-                if(ch == '\n')
+                if(ch == '\n') //if it's a new line, increment line number and continue
                 {
-                        lineno++;
+                        lineno++; 
                         continue;
                 }
-                else if(ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == '%')
+                else if(ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == '%') //if it's an arithmetic operator, increment token number and printf in output file
                 {
                         tokenno++;
                         fprintf(fp2,"%d\t\t %d\t\t %s\t\t %c\n",lineno, tokenno, arithop, ch);
                         continue;
                 }
-                else if(ch == '=')
+                else if(ch == '=') //if it's equals sign, firsth check if next char is also equals
                 {
-                        tokenno++;
-                        if((ch=fgetc(fp1)) == '=')
+                        tokenno++; //increment token number
+                        if((ch=fgetc(fp1)) == '=') //if next char also equals, then comparision op, else  assignment op
                         {
                                 fprintf(fp2,"%d\t\t %d\t\t %s\t\t ==\n",lineno, tokenno, compare);
                         }
@@ -43,7 +43,7 @@ int main()
                         }
                         continue;
                 }
-                else if(ch == ';')
+                else if(ch == ';') //if it's a delimiter, increment token number and printf in output file
                 {
                         tokenno++;
                         fprintf(fp2,"%d\t\t %d\t\t %s\t\t\t %c\n",lineno, tokenno, delim, ch);
@@ -52,8 +52,8 @@ int main()
 
         }
 
-        fclose(fp1);
-        fclose(fp2);
+        fclose(fp1); //close input file pointer
+        fclose(fp2); //close output file pointer
 
         return 0;
 }
